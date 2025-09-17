62575684ec9d4e35b12ad33ffe64ca55c9d36f63
Winky <mussarasul0501@gmail.com>
2025-09-17 21:48:47 +0300

    Page object prelimanary tests

---
+++ test_main_page/pages/base_page.py
@@ -0,0 +1,24 @@
+from selenium.common.exceptions import NoSuchElementException
+
+class BasePage():
+
+    def __init__(self, browser, url, timeout):
+        """Создание объекта веб клиента"""
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def open(self):
+        """Открывает указанную страницу в браузера"""
+        self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        """Булевый сёрчер по заданному методу поиска"""
+        try:
+            self.browser.find_element(how, what)
+
+        except NoSuchElementException:
+            return False
+
+        else:
+            return True
